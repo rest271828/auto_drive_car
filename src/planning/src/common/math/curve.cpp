efb9ba70a615d797a869ffd8fba3c9291240a7dd
rest271828 <3136427472@qq.com>
2026-05-21 23:18:22 +0800

    cartesian_to_frenet

--- src/planning/src/common/math/curve.cpp
+++ src/planning/src/common/math/curve.cpp
@@ -2,6 +2,72 @@
 
 namespace Planning
 {
+        double Curve::NormalizeAngle(const double &angle)
+        {
+                double a = std::fmod(angle + M_PI, 2.0 * M_PI);
+                if (a < 0.0)
+                {
+                        a += (2.0 * M_PI);
+                }
+                return a - M_PI;
+        }
+
+        void Curve::cartesian_to_frenet(const double &x, const double &y, const double &theta,
+                                        const double &speed, const double &a, const double &kappa, // 输入1: 目标点在笛卡尔下的参数:
+                                        const double &rs, const double &rx, const double &ry,
+                                        const double &rtheta, const double &rkappa, const double &rdkappa, // 输入2: 目标点在参考线
+                                        double &s, double &ds_dt, double &dds_dt,
+                                        double &l, double &dl_ds, double &dl_dt, double &ddl_ds, double &ddl_dt)
+        {
+                // 计算s
+                s = rs;
+                
+                // 计算l
+                const double dx = x - rx;
+                const double dy = y - ry;
+
+                const double cos_theta_r = std::cos(rtheta);
+                const double sin_theta_r = std::sin(rtheta);
+
+                const double cross_r_x = cos_theta_r * dy - sin_theta_r * dx;
+                l = std::copysign(std::hypot(dx, dy), cross_r_x);
+
+                // 计算l' = dl/ds
+                const double delta_theta = theta - rtheta;
+                const double tan_delta_theta = std::tan(delta_theta);
+                const double cos_delta_theta = std::cos(delta_theta);
+                const double sin_delta_theta = std::sin(delta_theta);
+                const double one_minus_kappa_l = 1 - rkappa * l;
+                dl_ds = one_minus_kappa_l * tan_delta_theta;
+
+                // 计算l'' = d(dl)/ds
+                const double kappa_l_prime = rdkappa * l + rkappa * dl_ds;
+                const double delta_theta_prime = one_minus_kappa_l / cos_delta_theta * kappa - rkappa;
+                ddl_ds = -kappa_l_prime * tan_delta_theta +
+                         one_minus_kappa_l / (cos_delta_theta * cos_delta_theta) * delta_theta_prime;
+
+                // 计算ds/dt
+                ds_dt = speed * cos_delta_theta / one_minus_kappa_l;
+
+                // 计算d(ds)/dt
+                dds_dt = (a * cos_delta_theta - (ds_dt * ds_dt) * (dl_ds * delta_theta_prime - kappa_l_prime)) / one_minus_kappa_l;
+
+                // 计算dl_dt
+                dl_dt = speed * sin_delta_theta;
+
+                // 计算ddl_dt
+                ddl_dt = a * sin_delta_theta;
+        }
+
+        void Curve::frenet_to_cartesian(const double &s, const double &ds_dt, const double &dds_dt,
+                                        const double &l, const double &dl_ds, const double &ddl_ds, // 输入1: 目标点在frenet下的参数
+                                        const double &rs, const double &rx, const double &ry,
+                                        const double &rtheta, const double &rkappa, const double &rdkappa, // 输入2: 目标点在参考线
+                                        double &x, double &y, double &theta,
+                                        double &speed, double &a, double &kappa)
+        {
+        }
+
         int Curve::find_match_point(const Path &path, const int &last_match_point_index, const PoseStamped &target_point)
         {
                 const int path_size = path.poses.size();
@@ -29,6 +95,17 @@ namespace Planning
                         }
                 }
                 return closest_index;
+        }
+        // 找匹配点下标（参考线）
+        int Curve::find_match_point(const Referline &refer_line, const PoseStamped &target_point)
+        {
+                return 0;
+        }
+
+        void Curve::find_projection_point(const Referline &path, const PoseStamped &target_point,
+                                          double &rs, double &rx, double &ry,
+                                          double &rtheta, double &rkappa, double &rdkappa)
+        {
         }
 
         // 计算投影点参数
--- src/planning/src/common/math/curve.h
+++ src/planning/src/common/math/curve.h
@@ -20,10 +20,34 @@ namespace Planning
     public:
         Curve() = default;
 
+        static double NormalizeAngle(const double &angle); // 角度归一化到[-pi, pi]
+
+        // 笛卡尔转frenet
+        static void cartesian_to_frenet(const double &x, const double &y, const double &theta,
+                                        const double &speed, const double &a, const double &kappa, // 输入1: 目标点在笛卡尔下的参数:
+                                        const double &rs, const double &rx, const double &ry,
+                                        const double &rtheta, const double &rkappa, const double &rdkappa, // 输入2: 目标点在参考线
+                                        double &s, double &ds_dt, double &dds_dt,
+                                        double &l, double &dl_ds, double &dl_dt, double &ddl_ds, double &ddl_dt); // 输出: 目标点在
+
+        // frenet转笛卡尔
+        static void frenet_to_cartesian(const double &s, const double &ds_dt, const double &dds_dt,
+                                        const double &l, const double &dl_ds, const double &ddl_ds, // 输入1: 目标点在frenet下的参数
+                                        const double &rs, const double &rx, const double &ry,
+                                        const double &rtheta, const double &rkappa, const double &rdkappa, // 输入2: 目标点在参考线
+                                        double &x, double &y, double &theta,
+                                        double &speed, double &a, double &kappa); // 输入出: 目标点在笛卡尔下的参数: x, y, theta, kap
+
         constexpr static double kMathEpsilon = 1.0e-6; // 小数
 
         // 找匹配点下标
         static int find_match_point(const Path &path, const int &last_match_point_index, const PoseStamped &target_point);
+        static int find_match_point(const Referline &refer_line, const PoseStamped &target_point);
+
+        // 找到投影点
+        static void find_projection_point(const Referline &path, const PoseStamped &target_point,
+                                          double &rs, double &rx, double &ry,
+                                          double &rtheta, double &rkappa, double &rdkappa); // 输出：投影点的rs,rx,ry,rtheta,rkappaco
 
         // 计算投影点参数
         static void cal_projection_param(Referline &refer_line);
